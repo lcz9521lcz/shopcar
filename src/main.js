aa96cb44e7795825e3523bc36ff8ea5626b216f6
lcz9521lcz <1254272059@qq.com>
2019-01-12 20:47:38 +0800

    轮播图完成

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
 import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css';
+import 'mint-ui/lib/style.css'
 
-Vue.use(MintUI);
+Vue.use(MintUI)
 
 //导入mui字体图标的css
 import './lib/mui/css/mui.css'
 import './lib/mui/css/icons-extra.css'
+
+//导入vue-resource请求接口获取数据
+import VueResource from 'vue-resource'
+//注册---在此之后才可以使用Vue.$http对象
+Vue.use(VueResource)
+
+//配置根域名
+Vue.http.options.root = 'http://www.lovegf.cn:8899/'
+
+//配置默认的post提交数据类型为传统表单
+Vue.http.options.emulateJSON = true
 
 
 Vue.config.productionTip = false
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,88 @@
 <template>
-  <div class="home-container">这是首页</div>
+  <div class="home-container">
+    <!-- 轮播图区域 -->
+    <mt-swipe :auto="2000" :speed="300">
+      <mt-swipe-item v-for="(item,index) in carousel" :key="index">
+        <a :href="item.url">
+          <img :src="item.img" alt>
+        </a>
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <!-- 九宫格区域 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-home"></span>
+          <div class="mui-media-body">Home</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-email">
+            <span class="mui-badge">5</span>
+          </span>
+          <div class="mui-media-body">Email</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-chatbubble"></span>
+          <div class="mui-media-body">Chat</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-location"></span>
+          <div class="mui-media-body">location</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-search"></span>
+          <div class="mui-media-body">Search</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <span class="mui-icon mui-icon-phone"></span>
+          <div class="mui-media-body">Phone</div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      carousel: []
+    };
+  },
+  created() {
+    this.getCarousel();
+  },
+  methods: {
+    getCarousel() {
+      this.$http.get("api/getlunbo").then(result => {
+        if (result.body.status === 0) {
+          this.carousel = result.body.message;
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  .mint-swipe {
+    height: 200px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
